999f16e0a5fd74897120c6394660f7984423ccca
SkIym <abramjosh13@gmail.com>
2024-07-20 12:11:55 +0800

    test: add form testids

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -28,15 +28,15 @@ const BlogForm = ({ addBlog }) => {
             <form onSubmit={uploadBlog} className="blog-form">
                 <div>
                     <span>Title:</span>
-                    <input type="text" name="text" value={newBlog.title} onChange={(e) => setNewBlog({ ...newBlog, title: e.target.value })} placeholder='Title'/>
+                    <input type="text" name="text" value={newBlog.title} onChange={(e) => setNewBlog({ ...newBlog, title: e.target.value })} placeholder='Title' data-testid='title'/>
                 </div>
                 <div>
                     <span>Author:</span>
-                    <input type="text" name="author" value={newBlog.author} onChange={(e) => setNewBlog({ ...newBlog, author: e.target.value })} placeholder='Author'/>
+                    <input type="text" name="author" value={newBlog.author} onChange={(e) => setNewBlog({ ...newBlog, author: e.target.value })} placeholder='Author' data-testid='author'/>
                 </div>
                 <div>
                     <span>Link:</span>
-                    <input type="text" name="url" value={newBlog.url} onChange={(e) => setNewBlog({ ...newBlog, url: e.target.value })} placeholder='URL'/>
+                    <input type="text" name="url" value={newBlog.url} onChange={(e) => setNewBlog({ ...newBlog, url: e.target.value })} placeholder='URL' data-testid='link'/>
                 </div>
                 <button type="submit">Create</button>
             </form>
